5cbc6b477d046c15527dbd79f6be921ad24c4a7b
magdalena-micic <mmicic@itekako.com>
2016-03-29 14:42:30 +0200

    Promenjena struktura backend direktorijuma

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
-
 """
-WSGI config for rezervoar-backend project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.5/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rezervoar-backend.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
