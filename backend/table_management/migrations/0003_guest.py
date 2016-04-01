a081b4605dff2b87ed7b133cff5e2f503dfdfaeb
panucci <pana2308@hotmail.com>
2016-04-01 17:49:52 +0200

    Dodate migracije za goste

---
+++ backend/table_management/migrations/0003_guest.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('table_management', '0002_auto_20160401_1543'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Guest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=128)),
+                ('last_name', models.CharField(max_length=128)),
+                ('phone_number', models.CharField(max_length=15)),
+                ('email', models.CharField(max_length=128, null=True, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
