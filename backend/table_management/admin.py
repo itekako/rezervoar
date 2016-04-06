aa6f1c28a2acf8e08e2f7687a6172e574f357099
panucci <pana2308@hotmail.com>
2016-04-06 14:17:06 +0200

    Dodat Reservation model

--- backend/table_management/admin.py
+++ backend/table_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from table_management.models import Table, Level, Guest
+from table_management.models import Table, Level, Guest, Reservation
 
 admin.site.register(Table)
 admin.site.register(Level)
 admin.site.register(Guest)
+admin.site.register(Reservation)
--- backend/table_management/models.py
+++ backend/table_management/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 from wheel.metadata import unique
 from datetime import datetime
-
 
 class Guest(models.Model):
     first_name = models.CharField(max_length=128)
@@ -14,6 +14,21 @@ class Guest(models.Model):
     def __unicode__(self):
         return self.first_name + " " + self.last_name
 
+class Reservation(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    id_guest = models.ForeignKey(Guest)
+    tables = models.CharField(max_length=128)
+    number_of_guests = models.IntegerField(null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    id_original = models.ForeignKey('self', default=None, null=True, blank=True)
+    id_user = models.ForeignKey(User)
+    comment = models.CharField(max_length=256)
+    valid = models.IntegerField(default=1)
+    canceled = models.IntegerField(default=0)
+    
+    def __unicode__(self):
+        return self.tables
 
 class Level(models.Model):
     label = models.CharField(max_length=128, unique=True)
