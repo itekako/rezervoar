0e3426d39722d67928a9168dbc550a618d767e09
magdalena-micic <mmicic@itekako.com>
2016-04-11 10:55:06 +0200

    Dodata skica restorana i omoguceno pomeranje jednog stola po skici

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -22,4 +22,29 @@ angular.module('rezervoarApp')
     };
 
     $scope.getGuest();
+
+    $scope.dragStart = function(event) {
+        var style = window.getComputedStyle(event.target, null);
+        event.dataTransfer.setData('text/plain',
+        (parseInt(style.getPropertyValue('left'),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue('top'),10) - event.clientY));
+    };
+
+    $scope.dragOver = function(event) {
+        event.preventDefault();
+        return false;
+    };
+
+    $scope.drop = function(event) {
+        var offset = event.dataTransfer.getData('text/plain').split(',');
+        var dm = document.getElementById('dragme');
+        dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+        dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+        event.preventDefault();
+        return false;
+    };
+
+    var dm = document.getElementById('dragme');
+    dm.addEventListener('dragstart', $scope.dragStart, false);
+    document.body.addEventListener('dragover', $scope.dragOver, false);
+    document.body.addEventListener('drop', $scope.drop, false);
 }]);
